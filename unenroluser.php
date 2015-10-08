90a485f7e8f1dd2d8dd7415411e21a93fa3b64f6
Johannes Burk <me@jojoob.de>
2015-10-08 15:59:18 +0200

    Fix: use of capabilities from other enrolment plugins (manual, self) instead of enrol_apply capabilities.

--- unenroluser.php
+++ unenroluser.php
@@ -49,8 +49,8 @@ if ($course->id == SITEID) {
 
 // Obviously
 require_login($course);
-// Make sure the user can unenrol self enrolled users.
-require_capability("enrol/self:unenrol", get_context_instance(CONTEXT_COURSE, $course->id));
+// Make sure the user can unenrol users.
+require_capability("enrol/apply:unenrol", get_context_instance(CONTEXT_COURSE, $course->id));
 
 // Get the enrolment manager for this course
 $manager = new course_enrolment_manager($PAGE, $course, $filter);
